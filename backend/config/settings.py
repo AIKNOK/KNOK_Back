970155ed0bdfb6b23ebe40ccd19ce990366bade3
김태영 <kimxodud0823@naver.com>
2025-06-18 20:10:08 +0900

    allow all, worker 2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-acglm2hmr87(e-83fxvlq(reeayyih&s50qp1!3ilbj6!3(ymu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
